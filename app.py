df6eac031a7762cb8eecf60b37b363a6882975af
Chingunjav <chingynjav01@yahoo.com>
2020-01-26 12:26:53 +0800

    version 1

---
+++ app.py
@@ -0,0 +1,24 @@
+# a=1
+# b=2
+# c=a+b
+# print(c)
+# print(a+b)
+# import random
+# a=random.randrange(1,100)
+# print(a)
+# str
+# owog = "baatar"
+# # print(owog.replace("b","a"))
+# print(len(owog))
+# print(owog[2:5])
+a = "Hello, World!"
+# print(a[1])
+# print(a[2:5])
+# print(len(a))
+# print(a.lower())
+print(a.find("H"))
+print(a.find("Wo"))
+print(a.replace("H", "J"))
+print(a.upper())
+print(a.lower())
+print(len(a))
