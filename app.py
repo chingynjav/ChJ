c451e554bafb8dffdf70d53d9c00178ee5c50b17
chingun <chingynjav@gmail.com>
2020-01-26 13:24:50 +0800

    if hichel uzev

--- app.py
+++ app.py
@@ -1,24 +1,38 @@
-# a=1
-# b=2
-# c=a+b
-# print(c)
-# print(a+b)
-# import random
-# a=random.randrange(1,100)
-# print(a)
-# str
-# owog = "baatar"
-# # print(owog.replace("b","a"))
-# print(len(owog))
-# print(owog[2:5])
-a = "Hello, World!"
-# print(a[1])
-# print(a[2:5])
+# # a=1
+# # b=2
+# # c=a+b
+# # print(c)
+# # print(a+b)
+# # import random
+# # a=random.randrange(1,100)
+# # print(a)
+# # str
+# # owog = "baatar"
+# # # print(owog.replace("b","a"))
+# # print(len(owog))
+# # print(owog[2:5])
+# a = "Hello, World!"
+# # print(a[1])
+# # print(a[2:5])
+# # print(len(a))
+# # print(a.lower())
+# print(a.find("H"))
+# print(a.find("Wo"))
+# print(a.replace("H", "J"))
+# print(a.upper())
+# print(a.lower())
+# print(len(a))
+# b = 5
+# c = 3
 # print(len(a))
 # print(a.lower())
-print(a.find("H"))
-print(a.find("Wo"))
-print(a.replace("H", "J"))
-print(a.upper())
-print(a.lower())
-print(len(a))
+# print(a.upper())
+# a=2
+a=6
+b=2
+
+if a>b :
+    print("a ih")
+else:
+    print("b ih")
+    
---
+++ if.py
@@ -0,0 +1,27 @@
+# a=6
+# b=2
+
+# if a>b :
+#     print("a ih")
+# else:
+#     print("b ih")
+# a=int(input("a too: "))
+# b=int(input("b too: "))
+# if a==b:
+#     print("a болон b тоо тэнцүү")
+# else:
+#     print("Тэнцүү биш")
+
+# a=int(input("a too: "))
+# b=int(input("b too: "))
+# if a!=b:
+#     print("a болон b тоо тэнцүү")
+# else:
+#     print("Тэнцүү биш")
+
+a=int(input("a too: "))
+b=int(input("b too: "))
+if a>b:
+    print("a болон b тоо тэнцүү")
+else:
+    print("Тэнцүү биш")
